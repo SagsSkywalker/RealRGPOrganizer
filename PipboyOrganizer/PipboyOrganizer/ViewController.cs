9255e0f1fafe4c5a5a15f36d2230d1d57b83659c
Sergio Guillen <sagssky@gmail.com>
2018-06-12 15:15:47 -0500

    ViewController modified

--- PipboyOrganizer/PipboyOrganizer/ViewController.cs
+++ PipboyOrganizer/PipboyOrganizer/ViewController.cs
@@ -2,10 +2,18 @@
 
 using UIKit;
 
+using PipboyOrganizer.Controllers;
+using PipboyOrganizer.Models;
+using System.Collections.Generic;
+
 namespace PipboyOrganizer
 {
     public partial class ViewController : UIViewController
     {
+        public static UserController uc;
+        public static QuestController qc;
+        public static SkillController sc;
+
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -15,12 +23,49 @@ namespace PipboyOrganizer
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        public void Initialize(){
+            //Load User from Firebase
+            //In the meantime we create new user
+            User user = new User()
+            {
+                Username = "Skywalker",
+                UserLevel = 1,
+                Experience = 0,
+                UserSkills = new List<Skill>(){
+                    new Skill(){
+                        Name = "Lightsaber Combat",
+                        Description = "The proper ways to use a lightsaber",
+                        Level = 1
+                    },
+                    new Skill(){
+                        Name = "Droid Repair",
+                        Description = "Bleeep",
+                        Level = 1
+                    }
+                }
+            };
+            uc = new UserController(user);
+            qc = new QuestController();
+            sc = new SkillController();
+        }
+
+        public void LoadUserData(){
+            LblUserName.Text = uc.user.Username;
+            LblUserLevel.Text = uc.user.UserLevel.ToString();
+        }
+
+        public List<Skill> UserTopSkills(){
+            //TODO: Return a List of "Top Skills"
+            return null;
         }
     }
 }
